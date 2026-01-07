754c42afe01b54311c451703ad7088ad3ba12258
MSubhanAli4210 <subhanali4219@gmail.com>
2026-01-07 15:45:29 +0500

    herosection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+
+import Header from "@/components/header";
+import HeroSection from "@/components/heroSection";
+import { NavBar } from "@/components/navbar";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="bg-black min-h-screen">
+        <Header />
+        <NavBar />
+        <HeroSection />
+      </div>
+    </>
+  );
 }
